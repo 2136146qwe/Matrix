86dffc91500a556c1434893ebf0ba4ef6a0eb6b6
Nastassia Voropai <2136146qwe@gmail.com>
2022-06-23 17:05:56 +0300

    Add new matrix

---
+++ Matrix.java
@@ -0,0 +1,34 @@
+public class Matrix {
+    public static void main(String[] args) {
+        int matrix[][] = {{1, 2, 3, 4},
+                {5, 6, 7, 8},
+                {9, 1, 2, 3},
+                {4, 5, 6, 7}};
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+            }
+        }
+        System.out.println();
+        System.out.println("Начальная матрица");
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix.length; j++) {
+                System.out.printf("%4d", matrix[i][j]);
+            }
+            System.out.println();
+        }
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = i + 1; j < matrix.length; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        System.out.println("Обратная матрица");
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix.length; j++) {
+                System.out.printf("%4d", matrix[i][j]);
+            }
+            System.out.println();
+        }
+    }
+}
