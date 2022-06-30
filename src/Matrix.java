d14846a9bd72a48ca47c37c032674a43664f8d22
Nastassia Voropai <2136146qwe@gmail.com>
2022-06-30 12:45:55 +0300

    Add ArrayList

--- src/Matrix.java
+++ src/Matrix.java
@@ -1,32 +1,64 @@
+package src;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Matrix {
-    public static void main(String[] args) {
-        int matrix[][] = {{1, 2, 3, 4},
-                {5, 6, 7, 8},
-                {9, 1, 2, 3},
-                {4, 5, 6, 7}};
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[i].length; j++) {
-            }
-        }
-        System.out.println();
-        System.out.println("Начальная матрица");
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix.length; j++) {
-                System.out.printf("%4d", matrix[i][j]);
+    public static void main(String args[]) {
+
+        List<ArrayList<Integer>> mass = new ArrayList<>();
+
+        ArrayList<Integer> a1 = new ArrayList<Integer>();
+        ArrayList<Integer> a2 = new ArrayList<Integer>();
+        ArrayList<Integer> a3 = new ArrayList<Integer>();
+        ArrayList<Integer> a4 = new ArrayList<Integer>();
+
+
+        a1.add(1);
+        a1.add(2);
+        a1.add(3);
+        a1.add(4);
+
+        a2.add(5);
+        a2.add(6);
+        a2.add(7);
+        a2.add(8);
+
+        a3.add(9);
+        a3.add(1);
+        a3.add(2);
+        a3.add(3);
+
+        a4.add(4);
+        a4.add(5);
+        a4.add(6);
+        a4.add(7);
+
+
+        mass.add(a1);
+        mass.add(a2);
+        mass.add(a3);
+        mass.add(a4);
+
+
+        for (ArrayList obj : mass) {
+
+            ArrayList<Integer> temp = obj;
+
+            for (Integer job : temp) {
+                System.out.printf("%4d", job);
             }
             System.out.println();
         }
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = i + 1; j < matrix.length; j++) {
-                int temp = matrix[i][j];
-                matrix[i][j] = matrix[j][i];
-                matrix[j][i] = temp;
-            }
-        }
-        System.out.println("Обратная матрица");
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix.length; j++) {
-                System.out.printf("%4d", matrix[i][j]);
+        System.out.println("\nПеревернутая");
+
+
+        Collections.reverse(mass);
+        for (ArrayList obj : mass) {
+            ArrayList<Integer> temp = obj;
+            for (Integer job : temp) {
+                System.out.printf("%4d", job);
             }
             System.out.println();
         }
